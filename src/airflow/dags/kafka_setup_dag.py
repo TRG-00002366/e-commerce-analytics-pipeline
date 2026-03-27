6d7d1a6cbabe630f4a8b58c8008e91ebc1d346a7
Anas Ibrahim <78625924+ibrahim-anas@users.noreply.github.com>
2026-03-27 17:51:12 -0400

    Create airflow dag for Kafka

---
+++ src/airflow/dags/kafka_setup_dag.py
@@ -0,0 +1,67 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from kafka_jobs.producer import stream_events
+from kafka_jobs.create_topics import create_topic
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "rev",
+    "depends_on_past": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="kafka_setup_workflow",
+    default_args=default_args,
+    description="Create Kafka topic and launch producer.",
+    schedule_interval=None,
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=["kafka", "topic", "producer"],
+) as dag:
+
+    start = EmptyOperator(task_id="start")
+    
+    task_create_topic = PythonOperator(
+        task_id="create_topic",
+        python_callable=create_topic
+    )
+
+    task_run_producer = PythonOperator(
+        task_id="run_producer",
+        python_callable=stream_events
+    )
+
+    # task_run_consumer = SparkSubmitOperator(
+    #     task_id="run_consumer",
+    #     application="/opt/spark-apps/streaming/stream_consumer.py",
+    #     conn_id="spark_local",
+    #     packages="org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0",
+    #     conf={
+    #         "spark.master": "spark://spark-master:7077"
+    #     },
+    #     env_vars={
+    #         "KAFKA_SERVERS": "kafka:29092",
+    #         "BRONZE_PATH": "/opt/data/bronze",
+    #         "SILVER_PATH": "/opt/data/silver",
+    #         "CHECKPOINT_PATH": "/opt/data/checkpoint"
+    #     }
+    # )
+    
+    task_run_consumer = BashOperator(
+        task_id="run_streaming_consumer",
+        bash_command=(
+            "spark-submit "
+            "--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 "
+            "/opt/spark-apps/streaming/stream_consumer.py "
+            "--bootstrap-servers kafka:29092 --duration 120"
+        ),
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> task_create_topic >> task_run_producer >> task_run_consumer >> end
