d390fb7bf7371c8dfab3ff11e5f6b622f3efcb5d
Anas Ibrahim <78625924+ibrahim-anas@users.noreply.github.com>
2026-03-27 17:52:41 -0400

    Remove redundant code

--- src/airflow/dags/df_batch_etl_dag.py
+++ src/airflow/dags/df_batch_etl_dag.py
@@ -2,7 +2,6 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash import BashOperator
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from batch.df_etl import df_etl
 from datetime import datetime, timedelta
 
@@ -23,53 +22,26 @@ with DAG(
     tags=["spark", "batch", "etl"],
 ) as dag:
 
-    start = EmptyOperator(task_id="start", dag=dag)
-
-    # ✅ Check if Silver data exists
+    start = EmptyOperator(task_id="start")
+    
+    # Check if Silver data exists
     check_silver_data = BashOperator(
         task_id="check_silver_data",
         bash_command="""
         if [ ! -d "/opt/data/silver" ]; then
-            echo "❌ Silver data not found!"
+            echo "Silver data not found!"
             exit 1
         else
-            echo "✅ Silver data exists"
+            echo "Silver data exists"
         fi
         """,
-        dag=dag
     )
-
-    # ✅ Stable Spark submit (this is the key part)
-    # run_df_etl = BashOperator(
-    #     task_id="run_df_etl",
-    #     bash_command="""
-    #     echo "🚀 Starting Spark ETL job..."
-
-    #     /opt/spark/bin/spark-submit \
-    #     --master spark://spark-master:7077 \
-    #     --deploy-mode client \
-    #     /opt/batch/df_etl.py
-
-    #     echo "✅ Spark ETL completed"
-    #     """
-    # )
-
-    # run_df_etl = SparkSubmitOperator(
-    #     task_id="run_df_etl",
-    #     conn_id="spark-conn",
-    #     application="/opt/batch/df_etl.py",
-    #     conf={
-    #         "spark.master": "spark://spark-master:7077"
-    #     },
-    #     verbose=True,
-    #     dag=dag
-    # )
 
     run_df_etl = PythonOperator(
         task_id="run_df_etl",
         python_callable=df_etl
     )
 
-    end = EmptyOperator(task_id="end", dag=dag)
+    end = EmptyOperator(task_id="end")
 
     start >> check_silver_data >> run_df_etl >> end
