d7edec67b93e8f2f8e8dbf90a5153e9e8b2191f8
Anas Ibrahim <78625924+ibrahim-anas@users.noreply.github.com>
2026-03-27 17:54:11 -0400

    Reorganize code

--- src/kafka_jobs/create_topics.py
+++ src/kafka_jobs/create_topics.py
@@ -6,36 +6,27 @@ import os
 from kafka.admin import KafkaAdminClient, NewTopic
 from kafka.errors import TopicAlreadyExistsError
 
-# Define topics to create
-TOPICS = [
-    NewTopic(
-        name="order_events",
-        num_partitions=3,
-        replication_factor=1
-    )
-]
-
 # Create Kafka topics
-def create_topics():
+def create_topic():
 
     admin_client = KafkaAdminClient(
         bootstrap_servers=os.getenv("KAFKA_SERVERS", "kafka: 29092"),
         client_id="topic_creator"
     )
 
+    topic = NewTopic(name="order_events",
+                     num_partitions=3,
+                     replication_factor=1)
+
     try:
         admin_client.create_topics(
-            new_topics=TOPICS,
+            new_topics=[topic],
             validate_only=False
         )
-        print("Topics created successfully.")
+        print("Topic created successfully.")
 
     except TopicAlreadyExistsError:
-        print("One or more topics already exist.")
+        print("Topic already exists.")
 
     finally:
         admin_client.close()
-
-
-if __name__ == "__main__":
-    create_topics()
--- src/kafka_jobs/producer.py
+++ src/kafka_jobs/producer.py
@@ -5,69 +5,68 @@ Kafka producer for generating and streaming fake e-commerce order events to Kafk
 import os
 import json
 import random
-import time
 from datetime import datetime, timezone
 from kafka import KafkaProducer
 from faker import Faker
 
-fake = Faker()
-
-producer = KafkaProducer(
-    bootstrap_servers=os.getenv("KAFKA_SERVERS", "kafka: 29092"),
-    value_serializer=lambda v: json.dumps(v).encode("utf-8")
-)
-
-# Event types for orders
-EVENT_TYPES = [
-    "ORDER_CREATED",
-    "ORDER_CANCELLED",
-    "ORDER_RETURNED",
-    "PAYMENT_COMPLETED",
-    "PAYMENT_FAILED"
-]
-
-customer_segments = ["REGULAR", "PRIME", "VIP"]
-categories = ["Electronics", "Books", "Clothing", "Home", "Sports"]
-payment_methods = ["CREDIT_CARD", "DEBIT_CARD", "PAYPAL", "APPLE_PAY"]
-shipping_types = ["STANDARD", "EXPRESS", "PRIME_EXPRESS"]
-regions = ["US-East", "US-West", "EU-West", "AP-South"]
-
-# Generate a random order event
-def generate_event():
-
-    quantity = random.randint(1, 5)
-    unit_price = round(random.uniform(10, 200), 2)
-    discount = round(random.uniform(0, 10), 2)
-
-    return {
-        "event_id": f"EVT-{fake.unique.random_int(min=90000, max=99999)}",
-        "event_type": random.choice(EVENT_TYPES),
-        "order_id": f"ORD-{fake.random_int(min=10000, max=99999)}",
-        "customer_id": f"CUST-{fake.random_int(min=100, max=999)}",
-        "customer_segment": random.choice(customer_segments),
-
-        "product_id": f"PROD-{fake.random_int(min=10, max=99)}",
-        "product_name": fake.word().title(),
-
-        "category": random.choice(categories),
-        "quantity": quantity,
-        "unit_price": unit_price,
-        "discount": discount,
-
-        "payment_method": random.choice(payment_methods),
-        "shipping_type": random.choice(shipping_types),
-
-        "region": fake.state_abbr(),
-
-        "timestamp": datetime.now(timezone.utc).isoformat()
-    }
-
-# Continuously send events to Kafka
 def stream_events():
+
+    fake = Faker()
+
+    producer = KafkaProducer(
+        bootstrap_servers=os.getenv("KAFKA_SERVERS", "kafka:29092"),
+        value_serializer=lambda v: json.dumps(v).encode("utf-8")
+    )
+
+    # Event types for orders
+    EVENT_TYPES = [
+        "ORDER_CREATED",
+        "ORDER_CANCELLED",
+        "ORDER_RETURNED",
+        "PAYMENT_COMPLETED",
+        "PAYMENT_FAILED"
+    ]
+
+    customer_segments = ["REGULAR", "PRIME", "VIP"]
+    categories = ["Electronics", "Books", "Clothing", "Home", "Sports"]
+    payment_methods = ["CREDIT_CARD", "DEBIT_CARD", "PAYPAL", "APPLE_PAY"]
+    shipping_types = ["STANDARD", "EXPRESS", "PRIME_EXPRESS"]
+
+    # Generate a random order event
+    def generate_event():
+
+        quantity = random.randint(1, 99)
+        unit_price = round(random.uniform(10, 200), 2)
+        discount = round(random.uniform(0, 10), 2)
+
+        return {
+            "event_id": f"EVT-{fake.unique.random_int(min=90000, max=9_999_999)}",
+            "event_type": random.choice(EVENT_TYPES),
+            "order_id": f"ORD-{fake.random_int(min=999, max=9_999_999)}",
+            "customer_id": f"CUST-{fake.random_int(min=100, max=9999)}",
+            "customer_segment": random.choice(customer_segments),
+
+            "product_id": f"PROD-{fake.random_int(min=10, max=9_999_999)}",
+            "product_name": fake.word().title(),
+
+            "category": random.choice(categories),
+            "quantity": quantity,
+            "unit_price": unit_price,
+            "discount": discount,
+
+            "payment_method": random.choice(payment_methods),
+            "shipping_type": random.choice(shipping_types),
+
+            "region": fake.state_abbr(),
+
+            "timestamp": datetime.now(timezone.utc).isoformat()
+        }
 
     print("Starting Kafka event stream (1 event/sec)...")
 
-    while True:
+    max_events = 10
+
+    for _ in range(max_events):
 
         event = generate_event()
 
@@ -78,14 +77,7 @@ def stream_events():
         )
 
         print(f"Sent {event['event_type']} | {event['event_id']}")
-
-        time.sleep(1)
-
-
-if __name__ == "__main__":
-    try:
-        stream_events()
-    except KeyboardInterrupt:
-        print("\nStopping producer...")
-        producer.flush()
-        producer.close()
+    
+    print("Stopping producer...")
+    producer.flush()
+    producer.close()
