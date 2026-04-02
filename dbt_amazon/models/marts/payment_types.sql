cab7acc5293c5b92c52cd883f31660b7df7aab40
aditisingh2019 <aditisingh2019@gmail.com>
2026-04-02 18:05:57 -0400

    payment_types fix

--- dbt_amazon/models/marts/payment_types.sql
+++ dbt_amazon/models/marts/payment_types.sql
@@ -1,12 +1,16 @@
 {{ config(materialized='table') }}
 
+WITH base AS (
+    SELECT
+        payment_method,
+        COUNT(*) AS payment_count
+    FROM {{ ref('stg_orders') }}
+    WHERE event_type = 'PAYMENT_COMPLETED'
+    GROUP BY payment_method
+)
+
 SELECT
-    event_type,
     payment_method,
-    COUNT(*) AS payment_count
-
-FROM {{ ref('stg_orders') }}
-
-WHERE event_type = 'PAYMENT_COMPLETED'
-
-GROUP BY event_type
+    payment_count,
+    payment_count * 1.0 / SUM(payment_count) OVER () AS pct_of_total
+FROM base
